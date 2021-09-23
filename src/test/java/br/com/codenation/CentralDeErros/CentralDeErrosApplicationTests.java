c2734570f1db69d7cb07007d7ce180b443c3b408
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-23 07:20:25 -0300

    First successful test

--- src/test/java/br/com/codenation/CentralDeErros/CentralDeErrosApplicationTests.java
+++ src/test/java/br/com/codenation/CentralDeErros/CentralDeErrosApplicationTests.java
@@ -1,14 +1,105 @@
 package br.com.codenation.CentralDeErros;
 
-import org.junit.Test;
-//import org.junit.jupiter.api.Test;
+import br.com.codenation.CentralDeErros.controller.ErrorEventLogController;
+import br.com.codenation.CentralDeErros.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
+import static br.com.codenation.CentralDeErros.enums.Roles.DEVELOPER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class CentralDeErrosApplicationTests {
 
+	@Autowired
+	private ErrorEventLogController errorEventLogController;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+//	@Autowired
+//	private WebApplicationContext webApplicationContext;
+
+
+	@Autowired
+	private MockMvc mockMvc;
+//
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private static final String CLIENT_ID = "quero-quero";
+	private static final String CLIENT_SECRET = "qu3r0-qu3r0";
+
+	private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+	private static final String EMAIL = "admin";
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(springSecurityFilterChain).build();
+	}
+//	@Before
+//	public void init() throws Exception {
+//		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+//	}
+
+
+	private String obtainAccessToken(String username, String password) throws Exception {
+		final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "password");
+		params.add("client_id", CLIENT_ID);
+		params.add("username", username);
+		params.add("password", password);
+
+		ResultActions result = mockMvc.perform(post("/oauth/token")
+						.params(params)
+						.with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+						.accept(CONTENT_TYPE))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(CONTENT_TYPE));
+
+		String resultString = result.andReturn().getResponse().getContentAsString();
+
+		JacksonJsonParser jsonParser = new JacksonJsonParser();
+		return jsonParser.parseMap(resultString).get("access_token").toString();
+	}
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(errorEventLogController).isNotNull();
+	}
+
+	@Test
+	public void givenNoToken_whenGetSecureRequest_thenUnauthorized() throws Exception {
+		mockMvc.perform(get("/event").param("description", "teste")).andExpect(status().isUnauthorized());
 	}
 
 }
