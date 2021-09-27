5361e0662e3bbc2b520651f08390ee08109badb0
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-26 21:51:55 -0300

    try insert table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,2 @@
-INSERT INTO error_event_log VALUES
-(1, '2021-09-12T10:46:06', 'description1', 'WARNING', 'log1', 'origin1', 1);
-INSERT INTO error_event_log VALUES
-(2, '2021-09-13T11:46:06', 'description2', 'ERROR', 'log2', 'origin2', 2);
-INSERT INTO error_event_log VALUES
-(3, '2021-09-14T12:46:06', 'description3', 'ERROR', 'log3', 'origin3', 3);
-INSERT INTO error_event_log VALUES
-(4, '2021-09-15T13:46:06', 'description4', 'WARNING', 'log4', 'origin4', 3);
+INSERT INTO INSERT INTO error_event_log(level, log, description, origin, quantity)
+            VALUES ("ERROR", "teste", "teste", "teste", 2);
